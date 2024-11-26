abc266823be10d54251e6d76ed03a645f3b679dc
SAjmal16 <sanna.ajmal@gmail.com>
2024-11-26 19:55:59 +0000

    useful message

--- assignment_3.py
+++ assignment_3.py
@@ -6,5 +6,8 @@ df = pd.read_excel("group_results - Copy.xlsx")
 
 df = df.drop("Entry", axis=1)
 df = df.drop("Fastest Download", axis=1)
+df = df.drop("Slowest Download", axis=1)
+df = df.drop("Fastest Upload", axis=1)
+df = df.drop("Slowest Upload", axis=1)
 print(df.describe())
 
