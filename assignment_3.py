70a0b4d2247608acd17fae86b1d14c5e388dadd4
SAjmal16 <sanna.ajmal@gmail.com>
2024-11-26 17:03:25 +0000

    first commit

---
+++ assignment_3.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+# df = pd.read_excel("results2024-11-25-144.xlsx")
+
+df = pd.read_excel("group_results - Copy.xlsx")
+
+df = df.drop("Entry", axis=1)
+df = df.drop("Fastest Download", axis=1)
+print(df.describe())
+
